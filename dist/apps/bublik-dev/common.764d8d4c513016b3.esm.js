ef0a7d09e80c38b106a9e2a3d155a52455105b56
Danil Kostromin <Danil.Kostromin@oktetlabs.ru>
2022-08-03 08:10:31 +0300

    chore(release): v0.4.1-beta

---
+++ dist/apps/bublik-dev/common.764d8d4c513016b3.esm.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[592],{61661:(e,u,n)=>{n.d(u,{Z:()=>c});var r=n(83879),s=n(31434),a=n(87709);function c(e,u){(0,a.Z)(2,arguments);var n=(0,r.Z)(u),c=7*n;return(0,s.Z)(e,c)}}}]);
